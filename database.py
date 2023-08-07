41d712cc8eb6512a2bdef178e7d3cf246ef00e34
kmark-n <117804697+kmark-n@users.noreply.github.com>
2023-08-07 09:10:39 +0000

    created dynamic pages that display each job role

--- database.py
+++ database.py
@@ -16,3 +16,21 @@ def load_jobs_from_db():
     column_names = result.keys()
     jobs_list = [dict(zip(column_names, row)) for row in result.fetchall()]
     return jobs_list
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM jobs WHERE id = :val")
+        result = conn.execute(query, {"val": id})
+        row = result.fetchone()
+
+    if row is None:
+        return None
+    else:
+        column_names = result.keys()
+        return dict(zip(column_names, row))
+
+
+
+
+      
+      
